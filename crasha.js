8dc51e9ec9c12f087c30a298e3d9a45ad6e2e3f9
JoaoRSS <joaors987654321@gmail.com>
2024-07-12 23:40:21 -0300

    aplicacao upada

---
+++ crasha.js
@@ -0,0 +1,113 @@
+let spreadsheetId = '1x3GZTJPw8wpCR0sjYPrFRZlY7LXGHu7Yd8GMZAHO2vE';
+let posicaoUm = 0;
+let posicaoDois = posicaoUm + 9;
+let range = 'Turmasdb!A' + posicaoUm + ':C' + posicaoDois;
+let apiKey = 'AIzaSyBJf7yNdLqhaBRb5VK5gbqJ7CbR0Gu3oLY';
+
+let url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+
+let primeiraVez = 0
+
+function printHere() {
+    print()
+}
+
+function proximaPag() {
+    var estudante;
+    var matricula;
+    var img;
+
+   
+        posicaoUm = posicaoUm + 9;
+        posicaoDois = posicaoUm + 9;
+        if (posicaoUm < 0){
+            posicaoUm = 0
+            posicaoDois = posicaoUm + 9;
+        }
+    
+
+    range = 'Turmasdb!A' + posicaoUm + ':C' + posicaoDois;
+    url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+
+    var idI = 0;
+    var idE = 0;
+    var idM = 0;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(error => {
+                    throw new Error(`Erro: ${error.error.message}`);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            const values = data.values;
+            for (let i = 0; i < 9; i++) {
+
+                idI = idI + 1;
+                idE = idE + 10;
+                idM = idM + 100;
+
+                estudante = values[i][0];
+                matricula = values[i][1];
+                img = values[i][2];
+
+                document.getElementById(idI).src = img;
+                document.getElementById(idE).innerHTML = estudante;
+                document.getElementById(idM).innerHTML = matricula;
+            }
+        });
+}
+
+function anteriorPag() {
+    var estudante;
+    var matricula;
+    var img;
+
+   
+        posicaoUm = posicaoUm - 9;
+        posicaoDois = posicaoUm + 9;
+        if (posicaoUm < 0){
+            posicaoUm = 9
+            posicaoDois = posicaoUm + 9;
+        }
+    
+
+    range = 'Turmasdb!A' + posicaoUm + ':C' + posicaoDois;
+    url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+
+    var idI = 0;
+    var idE = 0;
+    var idM = 0;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(error => {
+                    throw new Error(`Erro: ${error.error.message}`);
+                });
+            }
+            return response.json();
+        })
+        .then(data => {
+            const values = data.values;
+            for (let i = 0; i < 9; i++) {
+
+                idI = idI + 1;
+                idE = idE + 10;
+                idM = idM + 100;
+
+                estudante = values[i][0];
+                matricula = values[i][1];
+                img = values[i][2];
+
+                document.getElementById(idI).src = img;
+                document.getElementById(idE).innerHTML = estudante;
+                document.getElementById(idM).innerHTML = matricula;
+            }
+        });
+
+}
+
